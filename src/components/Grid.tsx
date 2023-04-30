98b1f9ea681b9731a1e45e9f11f38af160a7cccd
Daniel Villaverde <52183446+JDanielV@users.noreply.github.com>
2023-04-30 13:10:19 -0700

    progress with refactor

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -2,31 +2,14 @@ import React, { useState, useEffect, useCallback } from "react";
 import { GridItem } from "./GridItem";
 
 type GridProps = {
-  numItems: number;
+  numbersArr: number[];
 };
 
-export const Grid: React.FunctionComponent<GridProps> = ({ numItems }) => {
-  const populateItemsArr = useCallback((): number[] => {
-    const arr = [];
-    for (let i = 0; i < numItems + 1; i++) {
-      arr.push(i + 1);
-    }
-    return arr;
-  }, [numItems]);
-
-  const generateRandomNumber = (maxNum: number): number =>
-    Math.floor(Math.random() * (maxNum - 1)) + 1;
-
-  const [itemsArr, setItemsArr] = useState<number[]>(populateItemsArr());
-
-  useEffect(() => {
-    setItemsArr(populateItemsArr());
-  }, [numItems, populateItemsArr]);
-
+export const Grid: React.FunctionComponent<GridProps> = ({ numbersArr }) => {
   return (
     <div className="grid">
-      {itemsArr.map((item) => (
-        <GridItem key={item} numItem={generateRandomNumber(numItems)} />
+      {numbersArr.map((number) => (
+        <GridItem key={number} numItem={number} />
       ))}
     </div>
   );
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,9 +3,55 @@ import { Grid } from "./Grid";
 
 export const Main: React.FunctionComponent = () => {
   const [numItems, setNumItems] = useState<number>(5);
+  const [maxRandomNum, setMaxRandomNum] = useState<number>(32);
+  const [numGrids, setNumGrids] = useState<number>(60);
+  const [gridsArray, setGridsArray] = useState<number[][]>([]);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setNumItems(Number(e.target.value));
+    if (e.target.name === "number-items") {
+      setNumItems(Number(e.target.value));
+    } else if (e.target.name === "max-random-number") {
+      setMaxRandomNum(Number(e.target.value));
+    }
+  };
+
+  const generateRandomNumber = (maxNum: number): number =>
+    Math.floor(Math.random() * (maxNum - 1)) + 1;
+
+  const isGridUnique = (grid: number[]): boolean => {
+    let isUnique = true;
+
+    // comparison logic
+
+    return isUnique;
+  };
+
+  const populateNumbersArr = (): number[] => {
+    const arr: number[] = [];
+
+    for (let i = 0; i < numItems; i) {
+      let randomNumber = generateRandomNumber(maxRandomNum);
+
+      if (!arr.includes(randomNumber)) {
+        arr.push(randomNumber);
+        i++;
+      }
+    }
+
+    return arr;
+  };
+
+  const populateGridsArray = (): void => {
+    let newGrid: number[] = populateNumbersArr();
+
+    for (let i = 0; i < numGrids; i) {
+      if (isGridUnique(newGrid)) {
+        setGridsArray([...gridsArray, newGrid]);
+        i++;
+      } else {
+        newGrid = populateNumbersArr();
+      }
+    }
   };
 
   return (
@@ -21,9 +67,13 @@ export const Main: React.FunctionComponent = () => {
           placeholder="Enter multiple of 5"
           onChange={handleInputChange}
         />
+        <label htmlFor="max-random-number" className="main__input-label">
+          Max number
+        </label>
+        <input name="max-random-number" type="text" className="main__input" />
       </fieldset>
       {numItems != null && numItems > 4 && numItems % 5 === 0 ? (
-        <Grid numItems={numItems} />
+        gridsArray.map((grid) => <Grid numbersArr={grid} />)
       ) : (
         <span>Must be multiple of 5!!!!!!!!!</span>
       )}
